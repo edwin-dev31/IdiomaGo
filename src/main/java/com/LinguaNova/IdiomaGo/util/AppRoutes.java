48c06e00e24a1ebbedf59c423e79eec188270db3
Edwin <codeninja.hero@gmail.com>
2025-06-29 22:30:53 -0500

    mod: redirection frontend url

--- src/main/java/com/LinguaNova/IdiomaGo/util/AppRoutes.java
+++ src/main/java/com/LinguaNova/IdiomaGo/util/AppRoutes.java
@@ -5,7 +5,7 @@ public class AppRoutes {
     public static final String FRONTEND_BASE_URL = "https://delightful-field-0bf6cfa0f.2.azurestaticapps.net";
 
     public static final String VERIFY_EMAIL_ENDPOINT = BACKEND_BASE_URL + "/auth/verify-email?token=";
-    public static final String FRONTEND_REDIRECTION_URL = BACKEND_BASE_URL + "/oauth2/success?token=";
+    public static final String FRONTEND_REDIRECTION_URL = FRONTEND_BASE_URL + "/oauth2/success?token=";
 
     public static final String IMAGES_BASE_URL = "https://api.unsplash.com/search/photos";
     public static final String LANGUAGE_BASE_URL = "https://libretranslate.com/languages";
