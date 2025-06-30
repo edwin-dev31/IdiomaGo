36565ac586a17d9f2aba071c8b9f0e4163be1261
Edwin <codeninja.hero@gmail.com>
2025-06-29 21:46:54 -0500

    mod: change frontend url

--- src/main/java/com/LinguaNova/IdiomaGo/util/AppRoutes.java
+++ src/main/java/com/LinguaNova/IdiomaGo/util/AppRoutes.java
@@ -1,8 +1,8 @@
 package com.LinguaNova.IdiomaGo.util;
 
 public class AppRoutes {
-    public static final String BACKEND_BASE_URL = "http://localhost:1731/idiomago";
-    public static final String FRONTEND_BASE_URL = "http://localhost:5173";
+    public static final String BACKEND_BASE_URL = "https://idiomago-app-84aa3f1a7db9.herokuapp.com/idiomago";
+    public static final String FRONTEND_BASE_URL = "https://delightful-field-0bf6cfa0f.2.azurestaticapps.net";
 
     public static final String VERIFY_EMAIL_ENDPOINT = BACKEND_BASE_URL + "/auth/verify-email?token=";
     public static final String FRONTEND_REDIRECTION_URL = BACKEND_BASE_URL + "/oauth2/success?token=";
